635e5e4e3593ac76efed570c951cf3829040077c
Tung <tungto2010@gmail.com>
2019-08-23 01:39:46 +0700

    Update scan.py

--- core/scan.py
+++ core/scan.py
@@ -164,9 +164,9 @@ class Scan:
                 if info_gathering(self.url) is True:
                     # Vulnerabilities of target
                     print('\033[94m' + "==== Vulnerabilities ====" + '\033[94m')
-                    if "6.1.0" or "6.0.12" or "6.1.10" or "6.2.0" in version:
+                    if "6.1.0" or "6.0.12" or "6.1.10" or "6.2.0" in version or version is None:
                         json_api(self.url)
-                    if "6.1.0" in version:
+                    if "6.1.0" in version or version is None:
                         opensearch(self.url)
                     entry_point1 = self.url + "////api/liferay"
                     entry_point2 = self.url + "////api/spring"
